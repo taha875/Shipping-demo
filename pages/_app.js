e285dbc7d24ce29432d902c2a3a1d99191160b11
taha875 <tahaosama25@gmail.com>
2023-03-26 17:32:54 +0500

    test

--- pages/_app.js
+++ pages/_app.js
@@ -5,7 +5,7 @@ export default function App({ Component, pageProps }) {
   return (
     <>
       <Head>
-        <title>Inertia</title>
+        <title>XDEXPRESS</title>
         <meta />
         <link rel="preconnect" href="https://fonts.gstatic.com" />
         <link
